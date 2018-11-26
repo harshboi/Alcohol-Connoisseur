7260e8a2e989e02ea4054566805bd6e7297c4019
Sakthisa <Sakthisa@oregonstate.edu>
2018-11-26 11:02:39 -0800

    Pictures and title links to individual drink page

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" media="screen">
 
     <link rel="stylesheet" href="style.css">
-	
+
 	<script src="index.js" charset="utf-8" defer></script>
   </head>
   <body>
@@ -77,14 +77,14 @@
       </aside>
 
       <section id="posts">
-	  
+
  <?php
 		include 'db-password.php';  // this file includes the database password as a CONST DB_PASSWORD
         $host = 'classmysql.engr.oregonstate.edu';
         $db = 'cs340_schutfot';
         $user = 'cs340_schutfot';
         $charset = 'utf8mb4';
-		
+
 		$pass = DB_PASSWORD;
 
         // define database source name for accessing MariaDB
@@ -100,30 +100,32 @@
             $sql = "SELECT Drink.Drink_ID, Photo, Title, Description, Drink.Username, count(*) FROM Drink LEFT JOIN Likes ON Drink.Drink_ID = Likes.Drink_ID GROUP BY Drink.Drink_ID";
             $stmt = $pdo->prepare($sql);
             $stmt->execute();
-			
+
 			//For each drink fetch all information to be displayed on homepage
             while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-			
+
 				$title = $row['Title'];
 				$drink_id = $row['Drink_ID'];
 				$photo = $row['Photo'];
 				$description = $row['Description'];
 				$likes = $row['count(*)'];
 				$drinkcreater = $row['Username'];
-				
+
 				//If the photo doesn't exist, set as default image
 				if($photo == ""){
 					$photo = "cocktail.png";
 				}
-			
+
 			//Place the drink in the html as a post
 			echo"<div class='post' data-price='",$likes,"' data-city='",$description,"'>";
 			echo"<div class='post-contents'>";
             echo"<div class='post-image-container'>";
+      echo "<a href='drinkpage.php?did=",$drink_id,"'>";
 			echo"<img src='images/",$photo,"'>";
+      echo "</a>";
             echo "</div>";
             echo"<div class='post-info-container'>";
-			echo"<a href='drinkpage.php?did=",$drink_id,"' target='_blank' class='post-title'>",$title,"</a> <span class='post-price'>",$likes," Likes </span> <br><span class='post-city'>",$description,"</span>";
+			echo"<a href='drinkpage.php?did=",$drink_id,"' class='post-title'>",$title,"</a> <span class='post-price'>",$likes," Likes </span> <br><span class='post-city'>",$description,"</span>";
 			echo"<span> <br> Created by: ",$drinkcreater,"</span>";
 			echo"</div>";
 			echo"</div>";
@@ -135,7 +137,7 @@
 			$error_message = $e->getMessage();
             echo "<tr><td>", $error_message, "</td></tr>\n";
 		}
-                
+
 ?>
 
 
